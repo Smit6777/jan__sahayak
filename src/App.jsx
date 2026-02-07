602bce1f107e29d27499ec71ff172d6bee79a021
Smit6777 <25it052@charusat.edu.in>
2026-02-07 16:34:20 +0530

    Fix: Added basename to Router for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './index.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/form" element={<FormFiller />} />
